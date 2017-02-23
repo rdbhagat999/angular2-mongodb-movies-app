b420a74b1f78b3ae4ab16bff5e0a2bb14161eb7b
Ramandeep Bhagat <rdbhagat999@gmail.com>
2017-02-24 03:01:23 +0530

    updated movie-details component

--- src/app/components/movies/movie-details/movie-details.component.ts
+++ src/app/components/movies/movie-details/movie-details.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+import { Movie } from '../movie';
+import { MoviesService } from '../../../services/movies.service';
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.css']
 })
-export class MovieDetailsComponent implements OnInit {
+export class MovieDetailsComponent implements OnInit { 
 
-  constructor() { }
+	constructor(
+	  private route: ActivatedRoute,
+	  private router: Router,
+	  private _ms: MoviesService
+	) {}
 
-  ngOnInit() {
-  }
+	movie: Movie;
+
+	ngOnInit() {
+
+		//58949dd6734d1d3956c3cbe8
+
+	  /*this.route.params
+	    // (+) converts string 'id' to a number
+	    .switchMap((params: Params) => this.service.getHero(+params['id']))
+	    .subscribe((hero: Hero) => this.hero = hero);*/
+
+	  this.route.params
+	    // (+) converts string 'id' to a number
+	    .switchMap((params: Params) => this._ms.getMovie(params['id']))
+	    .subscribe((movie: Movie) => {
+	    	this.movie = movie;
+	    });
+
+
+	}
+
+
+
 
 }
