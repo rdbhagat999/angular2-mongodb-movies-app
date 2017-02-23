b9b59c2ea75ea185409ce5db9d461816429d5e8b
Ramandeep Bhagat <rdbhagat999@gmail.com>
2017-02-24 02:11:12 +0530

    updated movie class

--- src/app/components/movies/movie.ts
+++ src/app/components/movies/movie.ts
@@ -1,2 +1,7 @@
 export class Movie {
+
+	constructor( public title: string, public year: number, public description:string, actors: string[], public poster:string, public hero_image:string, public _id?: string, ) {
+
+	}
+
 }
